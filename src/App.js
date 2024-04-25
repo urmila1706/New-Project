4600e66399d15e0ba26df0870bd27b270a84a799
urmila1706 <raut.urmila88@gmail.com>
2024-04-25 13:56:53 +0530

    Avoid unnecessary re-renders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Button from './components/UI/Button/Button';
-
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-    {showParagraph &&<p>This is new!</p>}
+      <DemoOutput  />
     <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
